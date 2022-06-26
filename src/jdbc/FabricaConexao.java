7806bab10e8618fc7c8cc920c59effed72bc467d
Yuri Rampazo <ymrampazo@gmail.com>
2022-06-26 19:40:47 -0300

    JDBC: Criando tabela

--- src/jdbc/FabricaConexao.java
+++ src/jdbc/FabricaConexao.java
@@ -3,12 +3,11 @@ package jdbc;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
-import java.sql.Statement;
 
 public class FabricaConexao {
   public static Connection getConnection() throws SQLException {
     try {
-      final String url = "jdbc:mysql://localhost:3306?VerifyServerCertificate=false&useSSL=true";
+      final String url = "jdbc:mysql://localhost:3306/curso_java?VerifyServerCertificate=false&useSSL=true";
       final String user = "yuri";
       final String password = "123456";
 

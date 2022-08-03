c37845bb065d30ebda0c91b1013e2cf5771a3e62
Yuri Rampazo <ymrampazo@gmail.com>
2022-08-03 09:41:07 -0300

    Gerando JSON a partir de txt de lista de documentos

--- src/readersandwriters/TesteReaderAndWriter.java
+++ src/readersandwriters/TesteReaderAndWriter.java
@@ -15,7 +15,8 @@ public class TesteReaderAndWriter {
   public static void main(String[] args) throws IOException {
 
     BufferedReader br = new BufferedReader(new InputStreamReader(new FileInputStream("cpfs.txt")));
-    BufferedWriter bw = new BufferedWriter(new FileWriter("cpfsJSON.txt"));
+//    BufferedWriter bw = new BufferedWriter(new FileWriter("cpfsJSON.txt"));
+    BufferedWriter bw = new BufferedWriter(new FileWriter("cpfsJSON.json"));
 
     bw.write("{");
     bw.newLine();
